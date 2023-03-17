c2b2bceabb88d58f442fef11389b4f24f246e8e8
AdrianoC <adriano.cbagc@gmail.com>
2023-03-17 10:43:16 -0300

    Adicionei classificação

--- script.js
+++ script.js
@@ -3,27 +3,27 @@ let form = document.querySelector('.section__form')
 let btn = document.querySelector('.section__btn')
 let rating = document.querySelector('.section__rating')
 let url = document.querySelector('.section__url')
-let genero = document.querySelector('.section__genero')
+let ageRestriction = document.querySelector('#ageRestriction')
 
 
 rating.addEventListener('keyup',(e) => validarRating(e.target.value))
-genero.addEventListener('keyup',(e) => validarDesc(e.target.value))
+ageRestriction.addEventListener('keyup',(e) => validarAge(e.target.value))
 url.addEventListener('keyup',(e) =>validarUrl(e.target.value))
 btn.addEventListener('click',(e) =>adicionarCurso(e))
 
 
-let cursoData = { 
+const cursoData = { 
     titulo: '',
-    genero: '',
+    ageRestriction: '',
     url: ''
 }
-let ValidaCampos = () => {
+const ValidaCampos = () => {
     
         const ratingValido = rating.value.length >= 4;
-        const generoValida = genero.value.length >= 4;
+        const ageValida = ageRestriction.value.length >= 4;
         const urlValida = url.value.length >=4
       
-        if (ratingValido && generoValida && urlValida) {
+        if (ratingValido && ageValida && urlValida) {
 
           btn.disabled = false;
         } else {
@@ -40,19 +40,20 @@ let validarRating= (rating) => {
     ValidaCampos()
 }
 
-let validarDesc = (genero) => {
-    cursoData.genero= genero
+const validarAge = (ageRestriction) => {
+    cursoData.ageRestriction= ageRestriction
     ValidaCampos()
  
 
 }
-let validarUrl = (url) => {
+const validarUrl = (url) => {
     cursoData.url = url
     ValidaCampos()
 }
 
-let adicionarCurso = (e) => {
+const adicionarCurso = (e) => {
     e.preventDefault()
+
     cardRef.innerHTML += `
     <div class="card">
 
@@ -62,11 +63,9 @@ let adicionarCurso = (e) => {
 
         <div>
             <img src="${cursoData.url}" class="card__url">
+            <img src="${cursoData.ageRestriction}" class="card__ageRestriction">
         </div>
         
-        <div class="card__genero">
-            <p>${cursoData.genero}</p>
-        </div>
     </div>
 `
 form.reset()
